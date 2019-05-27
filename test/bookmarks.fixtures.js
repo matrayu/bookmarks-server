47193106b4d924e9afa48467c7926cbb682d15da
matrayu <northridge614@gmail.com>
2019-05-27 12:12:57 -0700

    Create bookmarks.fixtures.js

---
+++ test/bookmarks.fixtures.js
@@ -0,0 +1,29 @@
+function makeBookmarksArray() {
+    return [
+        {
+            "id" : 1,
+            "title" : "Thinkful",
+            "url" : "http:\/\/www.thinkful.com",
+            "description" : "Online coding bootcamp",
+            "rating" : "5"
+        },
+        {
+            "id" : 2,
+            "title" : "Google",
+            "url" : "http:\/\/www.google.com",
+            "description" : "Internet Search",
+            "rating" : "5"
+        },
+        {
+            "id" : 3,
+            "title" : "NBA",
+            "url" : "http:\/\/www.nba.com",
+            "description" : "National Basketball Association",
+            "rating" : "5"
+        }   
+    ]
+}
+
+module.exports = {
+    makeBookmarksArray,
+}
