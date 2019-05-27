f02a6c712f21af04818c63068edad6484e0034ef
matrayu <northridge614@gmail.com>
2019-05-27 12:12:45 -0700

    Create seed.bookmarks.sql

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,11 @@
+INSERT INTO bookmarks (title,url,description,rating) VALUES 
+('Thinkful','http://www.thinkful.com','Online coding bootcamp','5'),
+('Google','http://www.google.com','Internet Search','5'),
+('NBA','http://www.nba.com','National Basketball Association','5'),
+('MLB','http://www.mlb.com','Major League Baseball','3'),
+('Harmos','http://www.harmos.com','Music','5'),
+('Spotify','http://www.spotify.com','Music stream service','3'),
+('CNN','http://www.cnn.com','News','4'),
+('MSNBC','http://www.msnbc.com','News','2'),
+('Netflix','http://www.netflix.com','Video streaming service','5'),
+('KnexJs', 'https://knexjs.org', 'SQL query builder for Postgres', '3')
