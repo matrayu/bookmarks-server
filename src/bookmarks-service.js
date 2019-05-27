774d38aacb44aa5406af5c8950b17fc2b3e11d90
matrayu <northridge614@gmail.com>
2019-05-27 12:12:50 -0700

    Create bookmarks-service.js

---
+++ src/bookmarks-service.js
@@ -0,0 +1,31 @@
+const BookmarksService = {
+    getAllBookmarks(knex) {
+        return knex.select('*').from('bookmarks')
+    },
+    insertBookmark(knex, newBookmark) {
+        return knex
+            .insert(newBookmark)
+            .into('bookmarks')
+            .returning('*')
+            .then(rows => {
+                return rows[0];
+            });
+    },
+    getById(knex, id) {
+        return knex('bookmarks')
+            .where({ id })
+            .first()
+    },
+    deleteBookmark(knex, id) {
+        return knex('bookmarks')
+            .where({ id })
+            .delete()
+    },
+    updateBookmark(knex, id, updatedFields) {
+        return knex('bookmarks')
+            .where({ id })
+            .update(updatedFields)
+    },
+}
+
+module.exports = BookmarksService;
